6f07189e70192ba7ced95c5dce62b03fcc758d89
mohamadmelhem <mhamadmlhm0@gmail.com>
2023-06-01 13:17:04 +0300

    hello1

--- src/components/tourist/ResortProfile.js
+++ src/components/tourist/ResortProfile.js
@@ -2,19 +2,22 @@ import "./Body.css"
 import "react-multi-carousel/lib/styles.css";
 import { useState } from "react";
 import ContentResort from "./ContentResort";
+
 const ResortProfile = (props) => {
-    const multImages = props.data.images.split(",");
-    const [active, setActive] = useState(`https://hjezli-backend.onrender.com/${multImages[0]}`)
+    // const multImages = props.data.images.split(",");
+    const images = props.data.images;
+    console.log(images);
+    const [active, setActive] = useState(`${images[0]}`)
     return (<div className="resort-profile">
         <div>
             <div className="image">
                 <img src={active} alt="sss" />
             </div>
             <div className="resort-gallery">
-                {multImages && multImages.map((image) => (
-                    <img src={`https://hjezli-backend.onrender.com/${image}`} alt="resort-images" onClick={(e) => { setActive(e.target.src) }} key={image} />
+                {images && images.map((image) => (
+                    <img src={image} alt="resort-images" onClick={(e) => { setActive(e.target.src) }} key={image} />
                 ))}
-            </div>;
+            </div>
         </div>
 
         <ContentResort data={props.data} />
